d9e95597d0cfbc65004510a3621749e4677c315c
Rob Watson <rob.watson@me.com>
2023-02-08 10:01:45 +0000

    Hide main Todo page if not logged in

--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -5,9 +5,15 @@ import TodoOptions from "@/client/components/TodoOptions"
 import { useState } from "react"
 import Footer from "@/client/components/Footer";
 import Nav from "@/client/components/Nav/Nav";
+import useAuth from "@/client/Auth/Hooks/useAuth";
 
 export default function Home() {
   const [showCompleteTodos, setShowCompleteTodos] = useState(false)
+  const auth = useAuth()
+
+  if (!auth.isLoggedIn()) {
+    return null
+  }
 
   return (
     <>
